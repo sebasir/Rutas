494af01719a7b4713f753e824f2ba4b1ad7821c5
Sebastian Motavita <smotavitam@ucentral.edu.co>
2018-12-20 15:47:12 -0500

    removing deploy folder

--- out/artifacts/Rutas_war_exploded/markerAssets.js
+++
@@ -1,8 +0,0 @@
-const Icons = {
-    schoolBus: {text: '\uf55e', labelOrigin: {x: 194, y: 194}, fontSize: '2.1em', color: '#ffffff'},
-    person: {text: '\uf2bd', labelOrigin: {x: 194, y: 194}, fontSize: '2.9em', color: '#ffffff'}
-};
-
-const SvgPaths = {
-    defaultMarker: "M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z"
-};
